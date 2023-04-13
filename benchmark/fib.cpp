e7d3ce2902447038d7c496ed1803762fc992d15d
Weaverzhu <weaver_zhu@outlook.com>
2023-04-14 00:10:59 +0800

    add benchmark & result

--- benchmark/fib.cpp
+++ benchmark/fib.cpp
@@ -1,4 +1,5 @@
 #include "folly/executors/GlobalExecutor.h"
+#include "folly/executors/InlineExecutor.h"
 #include <benchmark/benchmark.h>
 #include <folly/experimental/coro/Collect.h>
 #include <folly/experimental/coro/Task.h>
@@ -48,9 +49,11 @@ folly::coro::Task<int> fib_coro(int n) {
   if (n <= 2) {
     co_return n;
   }
-  auto [res1, res2] = co_await folly::coro::collectAll(
-      fib_coro(n - 1).scheduleOn(folly::getGlobalCPUExecutor()),
-      fib_coro(n - 2).scheduleOn(folly::getGlobalCPUExecutor()));
+  auto [res1, res2] =
+      co_await folly::coro::collectAll(
+          fib_coro(n - 1).scheduleOn(folly::getGlobalCPUExecutor()),
+          fib_coro(n - 2).scheduleOn(folly::getGlobalCPUExecutor()))
+          .scheduleOn(&folly::InlineExecutor::instance());
   cpuTask();
   co_return res1 + res2;
 }
@@ -66,3 +69,17 @@ void BM_fib_coro(benchmark::State &state) {
 }
 
 BENCHMARK(BM_fib_coro)->Arg(15)->Arg(17)->Arg(20)->Arg(22);
+
+/*
+-------------------------------------------------------------
+Benchmark                   Time             CPU   Iterations
+-------------------------------------------------------------
+BM_fib_taskflow/15     941649 ns        49392 ns        10000
+BM_fib_taskflow/17    2216373 ns       122430 ns         5643
+BM_fib_taskflow/20    8633316 ns       509577 ns         1358
+BM_fib_taskflow/22   22272463 ns      1485570 ns          494
+BM_fib_coro/15         916313 ns         2444 ns        10000
+BM_fib_coro/17        2258221 ns         2580 ns        10000
+BM_fib_coro/20        9266028 ns         3183 ns         1000
+BM_fib_coro/22       24349100 ns         5430 ns         1000
+*/
